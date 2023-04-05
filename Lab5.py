3c2b6e2123d9a4c4a6b493064d988d726e814730
orangeistoxic <38318841+orangeistoxic@users.noreply.github.com>
2023-04-05 21:50:41 +0800

    132

---
+++ Lab5.py
@@ -0,0 +1,35 @@
+import math
+import sys
+"""
+This homework may need to use math library
+
+You need to fix #TODO part to finish homework
+test_number is your input
+you can test number and output in terminal by following command:
+"python Lab5.py (your number)"
+Ex:python Lab5.py 97
+"""
+
+test_number = int(sys.argv[1])
+print(test_number)
+#TODO part
+'''
+you need write 2 program(Ch03, page16)
+1.Naive algorithm
+2.Quick algorithm
+In this 2 program, you need to print "test_number is prime" if your input is a prime number
+and you need to print "not a prime number" if your input is not a prime number
+Hint:'break' can help you finish program faster
+
+Therefore, when you run "python Lab5.py 97", you should see the result is
+
+97
+test_number is prime
+test_number is prime
+
+Meanwhile, when you run "python Lab5.py 6", you should see the result is
+
+6
+not a prime number
+not a prime number
+'''
